03f229b15e0b8f3c699af302f164777f6114497a
qholzweg <qholzweg@gmail.com>
2021-10-14 16:18:56 +0300

    clear mock layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,11 +12,7 @@
   })
 </script>
 
-<div class="max-w-screen-lg mx-auto">
-	<header class="bg-yellow-50 max-w-full h-24 mb-10"></header>
-	<div class="grid grid-cols-4 gap-8">
-		<sidebar class="h md:col-span-1 bg-gray-50 rounded"></sidebar>
-		<article class="col-span-4 md:col-span-3">
+<div class="max-w">
 			<PageTransition refresh={key}>
 			<main id="main-container" class="max-w-full md:shadow-lg md:rounded-lg p-5 md:p-10 mb-10">
 				<h2 class="mb-5">
@@ -27,11 +23,5 @@
 			
 			</main>
 		</PageTransition>
-			<h1>Lorem ipsum</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quisquam fugit nam! Vel, officiis eum ratione quod, consequatur itaque aliquid laudantium in aspernatur minus quam, modi eaque delectus autem eos!</p>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quisquam fugit nam! Vel, officiis eum ratione quod, consequatur itaque aliquid laudantium in aspernatur minus quam, modi eaque delectus autem eos!</p>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quisquam fugit nam! Vel, officiis eum ratione quod, consequatur itaque aliquid laudantium in aspernatur minus quam, modi eaque delectus autem eos!</p>
-		</article>
-	</div>
 </div>
 
