ffe756ae573d9f02ad61b62d02c23fe69641f661
Alexander Grimalovsky <alexander.grimalovsky@gmail.com>
2024-02-17 20:05:28 +0300

    Fixed a11y issue reported by Svelte

--- src/lib/Country.svelte
+++ src/lib/Country.svelte
@@ -19,10 +19,22 @@
 {#if country.cities}
     <ul class="cities-list mb-7">
         {#each country.cities as city}
-            <li class="city py-0.5"><a href="#" on:click="{() => openCity(city)}" class="hover:underline">
-                {#if city.mark}
-                    <Fa icon={faStar} class="inline text-gray-300"/>
-                {/if}{city.name}</a></li>
+            <li class="city py-0.5">
+                <span
+                    on:click="{() => openCity(city)}"
+                    on:keypress="{() => openCity(city)}"
+                    class="hover:underline cursor-pointer"
+                    style="color: var(--link-color)"
+                    role="link"
+                    aria-roledescription="Link to the city information"
+                    tabindex="0"
+                >
+                    {#if city.mark}
+                        <Fa icon={faStar} class="inline text-gray-300"/>
+                    {/if}
+                    {city.name}
+                </span>
+            </li>
         {/each}
     </ul>
 {/if}
